0adcdb68b6cdd0066eea7d43b99868c2358be001
ツ Ruwantha-OFc <lasithruwantha3@gmail.com>
2026-06-29 17:44:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT||8080;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
